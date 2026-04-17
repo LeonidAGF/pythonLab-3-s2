45e7ca84797899e53a7ba2a18d293c033dda630a
Leonid Gurianov <leonid.11521@gmail.com>
2026-04-17 22:36:39 +0300

    new filtration

--- src/main.py
+++ src/main.py
@@ -2,8 +2,15 @@ from SourceFromFile import SourceFromFile
 from SourceFromGenerator import SourceFromGenerator
 from SourceFromWeb import SourceFromWeb
 from client import ClientGet as Client, ClientPost
+from task import Task
 from task_manager import task_manager
 from task_queue import TaskQueue
+
+
+def filter_by_status(task: Task):
+    if task.status == 'ready':
+        return True
+    return False
 
 
 def main() -> None:
@@ -11,9 +18,11 @@ def main() -> None:
     Обязательнная составляющая программ, которые сдаются. Является точкой входа в приложение
     :return: Данная функция ничего не возвращает
     """
-    client1 = Client("https://my.meteoblue.com/packages/basic-1h_basic-day?apikey=bOico7hWTVAzPQYM&lat=55.752&lon=37.6178&asl=155&format=json")
+    client1 = Client(
+        "https://my.meteoblue.com/packages/basic-1h_basic-day?apikey=bOico7hWTVAzPQYM&lat=55.752&lon=37.6178&asl=155&format=json")
     client2 = Client("https://ru.wikipedia.org/wiki/Python")
-    client3 = ClientPost("https://jsonplaceholder.typicode.com/posts",message={"prompt": "{title: 'foo',body: 'bar',userId: 1}"})
+    client3 = ClientPost("https://jsonplaceholder.typicode.com/posts",
+                         message={"prompt": "{title: 'foo',body: 'bar',userId: 1}"})
 
     source_from_web1 = SourceFromWeb(client1)
     source_from_web2 = SourceFromWeb(client2)
@@ -25,8 +34,12 @@ def main() -> None:
 
     tq = TaskQueue(source_from_web1)
 
-    for el in tq:
+    for el in list(tq.filtration(filter_by_status)):
         print(el)
+
+    sum_of_priority: int = sum(el.priority for el in tq)
+
+    print(sum_of_priority)
 
     print("Tasks from web:")
 
@@ -36,15 +49,16 @@ def main() -> None:
     for el in task_manager(source_from_web3):
         print(el)
 
-    print("\n","Tasks from file:")
+    print("\n", "Tasks from file:")
 
     for el in task_manager(source_from_file):
         print(el)
 
-    print("\n","Tasks from generators:")
+    print("\n", "Tasks from generators:")
 
     for el in task_manager(source_from_generators):
         print(el)
+
 
 if __name__ == "__main__":
     main()
--- src/task_queue.py
+++ src/task_queue.py
@@ -1,4 +1,4 @@
-from typing import Iterator
+from typing import Iterator, Callable, Any
 
 from Source import TaskSource
 from task import Task
@@ -9,13 +9,11 @@ class TaskQueue:
 
     """
 
-    def __init__(self, source: TaskSource,status:str='none',priopity:int = 0) -> None:
+    def __init__(self, source: TaskSource) -> None:
         """
 
         """
         self.source: TaskSource = source
-        self.status = status
-        self.priopity = priopity
 
     def __iter__(self) -> Iterator[Task]:
         """
@@ -23,8 +21,12 @@ class TaskQueue:
         """
 
         for task in self.source.get_tasks():
-           filtered:int = 1
-           if (self.status!=task.status and self.status!='none') or (self.priopity!=task.priority and self.priopity!=0):
-               filtered = 0
-           if filtered==1:
+            yield task
+
+    def filtration(self, func: Callable[[Task], bool]) -> Iterator[Task]:
+        """
+
+        """
+        for task in self:
+            if func(task):
                 yield task
